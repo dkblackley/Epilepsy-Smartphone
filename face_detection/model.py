f46aa26764587d4f090bd84fae367df75d28510f
Daniel <dkblackley@dundee.ac.uk>
2021-07-15 12:52:21 +0100

    Video splitting scripts

--- face_detection/model.py
+++ face_detection/model.py
@@ -1,10 +1,91 @@
-import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-import torchvision.datasets as datasets
+from facenet_pytorch import MTCNN
+import torch
+import numpy as np
+import mmcv, cv2
+import os
+from PIL import Image, ImageDraw
+from IPython import display
 
-num_classes = 2  # background and face
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
 
-data_set = datasets.WIDERFace(split='train', download=True)
+mtcnn = MTCNN(keep_all=True, device=device)
 
-# load a model pre-trained pre-trained on COCO
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+# Playing video from file:
+cap = cv2.VideoCapture('video.mp4')
+
+'''try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print ('Error: Creating directory of data')'''
+
+currentFrame = 0
+
+frames = []
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if currentFrame % 3 == 0:
+        currentFrame += 1
+        pass
+
+    frames.append(frame)
+
+    # Saves image of the current frame in jpg file
+    #name = './data/frame' + str(currentFrame) + '.jpg'
+    #print ('Creating...' + name)
+    #cv2.imwrite(name, frame)
+
+    # To stop duplicate images
+    currentFrame += 1
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+#video = mmcv.VideoReader('video.mp4')
+#frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in video]
+
+
+display.Video('video.mp4', width=640)
+
+frames_tracked = []
+for i, frame in enumerate(frames):
+    print('\rTracking frame: {}'.format(i + 1), end='')
+
+    # Detect faces
+    boxes, _ = mtcnn.detect(frame)
+
+    # Draw faces
+    frame_draw = frame.copy()
+    draw = ImageDraw.Draw(frame_draw)
+    for box in boxes:
+        draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
+
+    # Add to frame list
+    frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
+print('\nDone')
+
+
+d = display.display(frames_tracked[0], display_id=True)
+i = 1
+try:
+    while True:
+        d.update(frames_tracked[i % len(frames_tracked)])
+        i += 1
+except KeyboardInterrupt:
+    pass
+
+
+
+
+dim = frames_tracked[0].size
+fourcc = cv2.VideoWriter_fourcc(*'FMP4')
+video_tracked = cv2.VideoWriter('video_tracked.mp4', fourcc, 25.0, dim)
+for frame in frames_tracked:
+    video_tracked.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+video_tracked.release()
+
