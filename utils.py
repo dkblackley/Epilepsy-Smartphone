500721a11a50eb2c1f3e97139b5e25d5bdd0d7e7
Daniel <dkblackley@dundee.ac.uk>
2021-08-28 07:49:43 +0100

    more comments

--- utils.py
+++ utils.py
@@ -1,17 +1,26 @@
+"""
+utils.py - holds any helpful utility function for other files
+"""
+
 import os
 import cv2
 import csv
 import torch
-import numpy as np
-from PIL import Image, ImageDraw
-from epilepsy_classification.model import Classifier
+from PIL import Image
 import torch.optim as optim
 
+from epilepsy_classification.model import Classifier
+
 LABELS = {'INF': 0, 'MYC': 1}
-NUMBERS= {0: 'INF', 1: 'MYC'}
+NUMBERS = {0: 'INF', 1: 'MYC'}
+
 
 def make_labels(path):
-
+    """
+    Deprecated method used to strip previous dataset down to binary classifier
+    :param path: Path to the labels
+    :type path: str
+    """
     labels = [['video', 'chew', 'clap']]
 
     for direct in os.listdir(path):
@@ -32,55 +41,19 @@ def make_labels(path):
 
     write_to_csv(path + 'labels.csv', labels)
 
-def change_videos_fps(path_to_data):
-
-    filenames = [f for f in os.listdir(path_to_data) if os.path.isfile(os.path.join(path_to_data, f))]
-
-    for filename in filenames:
-
-        if filename[-4:] != '.mp4':
-            continue
-
-        filename = "spasm.mp4"
-        new_frames = []
-        frame_loop = 0
-
-        filename = path_to_data + filename
-
-        # Playing video from file:
-        cap = cv2.VideoCapture(filename)
-
-        while(True):
-
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-
-            if frame_loop >= 1:
-                frame_loop -= 1
-                continue
-
-            try:
-                frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            except:
-                break
-
-            new_frames.append(frame)
-            frame_loop += 2
-
-        dim = new_frames[0].size
-        fourcc = cv2.VideoWriter_fourcc(*'FMP4')
-        video_tracked = cv2.VideoWriter(filename, fourcc, 10, dim)
-
-        for frame in new_frames:
-            video_tracked.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
-        video_tracked.release()
-        cap.release()
 
 def change_into_frames(path_to_data, path_to_save, labels):
+    """
+    changes videos into just a bunch of images in a folder. Useful for the Pytorch VideoDataloader
+    :param path_to_data: path to the videos
+    :type path_to_data: str
+    :param path_to_save: path to the directory you wish to save the video frames to
+    :type path_to_save: str
+    :param labels: the list of labels for each video
+    :type labels: list
+    """
 
-    #filenames = [f for f in os.listdir(path_to_data) if os.path.isfile(os.path.join(path_to_data, f))]
     labels.pop(0)
-
     for i in range(0, len(labels)):
         ID = labels[i].index('1') - 1
         labels[i] = [labels[i][0], ID]
@@ -88,9 +61,7 @@ def change_into_frames(path_to_data, path_to_save, labels):
     annotations = []
 
     for filename, label in labels:
-
         frames = 0
-
         if filename[-4:] != '.mp4':
             continue
 
@@ -122,7 +93,7 @@ def change_into_frames(path_to_data, path_to_save, labels):
             frame_loop += 2
             frames += 1
 
-            frame.save(path_to_data + real_name + '/img_' + str(frames).zfill(5) + '.jpg')
+            frame.save(path_to_save + real_name + '/img_' + str(frames).zfill(5) + '.jpg')
 
         annotations.append([real_name + ' 1' + f' {frames} ' + str(label)])
         cap.release()
@@ -140,26 +111,44 @@ def write_to_csv(path, list_to_write):
         writer = csv.writer(f)
         writer.writerows(list_to_write)
 
+
 def read_from_csv(filename, to_num=False):
     """
     read a file of comma separated values
-    :return: a list of the read values
+    :param filename: path to the file to read
+    :type filename: str
+    :param to_num: Whether or not to read file as a string or list of floats
+    :type to_num: Bool
+    :return: the read csv
+    :rtype: list
     """
 
     list_to_return = []
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            for i in range(0, len(row)):
-                row[i] = float(row[i])
+            if to_num:
+                for i in range(0, len(row)):
+                    row[i] = float(row[i])
 
 
             list_to_return.append(row)
 
     return list_to_return
 
-def num_boxes_greater_than_ratio(boxes, debug=False, ratio=0.8):
 
+def num_boxes_greater_than_ratio(boxes, debug=False, ratio=0.8):
+    """
+    check if the number of boxes passed in are greater than the specified threshold
+    :param boxes: the bounding boxes
+    :type boxes: list
+    :param debug: Whether or not to print debug messages
+    :type debug: Bool
+    :param ratio: the ratio to determine how many boxes are acceptable
+    :type ratio: float
+    :return: True or False, depending on whether the number of valid boxes passes the threshold
+    :rtype: Bool
+    """
     total = len(boxes)
     invalid = 0
     valid = 0
@@ -179,8 +168,23 @@ def num_boxes_greater_than_ratio(boxes, debug=False, ratio=0.8):
     else:
         return False
 
-def save_results(path, losses, accuracies, loss_filename="losses", accuracry_filname="accuracy", debug=False):
 
+def save_results(path, losses, accuracies, loss_filename="losses", accuracry_filname="accuracy", debug=False):
+    """
+    saves the losses and accuracies of the network, useful for further training
+    :param path: path specifying where to save the data
+    :type path: str
+    :param losses: the list of losses
+    :type losses: list
+    :param accuracies: the list of accuracies
+    :type accuracies: list
+    :param loss_filename: the default filename for the loss file
+    :type loss_filename: str
+    :param accuracry_filname:  the default filename for the accuracy file
+    :type accuracry_filname: str
+    :param debug: Whether or not to print debug messages
+    :type debug: Bool
+    """
     if not os.path.isdir(path):
         os.makedirs(path)
     if debug:
@@ -189,7 +193,13 @@ def save_results(path, losses, accuracies, loss_filename="losses", accuracry_fil
     write_to_csv(path + f"{accuracry_filname}.csv", accuracies)
 
 def get_results(paths):
-
+    """
+    loads in the saved results
+    :param paths: paths to the accuracy and loss
+    :type paths: list
+    :return: the lists of accuracy and loss, returns in the same order you passed in
+    :rtype: list
+    """
     list = []
     for path in paths:
         list.append(read_from_csv(path, True))
@@ -197,6 +207,15 @@ def get_results(paths):
     return list
 
 def save_model(model, optim,  path):
+    """
+    Saves the Pytorch model and optimizer
+    :param model: The Pytorch Classifier
+    :type model: nn.Module
+    :param optim: The pytorch optimizer
+    :type optim: nn.optimizer
+    :param path: path to save model to
+    :type path: str
+    """
     if not os.path.isdir(path):
         os.makedirs(path)
 
@@ -206,7 +225,17 @@ def save_model(model, optim,  path):
     torch.save(states, path + "/model")
 
 def load_model(path, frame_segments, device):
-
+    """
+    Loads model and optimizer from specified path
+    :param path: path to model
+    :type path: str
+    :param frame_segments: number of frame segments to use
+    :type frame_segments: int
+    :param device: device to load model on to
+    :type device: str
+    :return: The model and the optimizer
+    :rtype: list
+    """
     net = Classifier(frame_segments, dropout=0.35)
     optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=0.0001)
     states = torch.load(path, map_location=device)
